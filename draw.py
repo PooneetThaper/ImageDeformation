9b555c64600f29e726f57a209fa116f2352f336b
jamesw8 <jameswong8@gmail.com>
2018-05-17 18:50:57 -0400

    Added ovals to depict before and after transformation and arrow indicator

--- draw.py
+++ draw.py
@@ -1,14 +1,56 @@
 import tkinter
 from PIL import ImageTk, Image
+import numpy
 import os
 
 # Listener callbacks
 def listenClick(event):
+	global current
 	print('Clicking', event.x, event.y)
+	for pt in new:
+		point = w.coords(pt)
+		if (event.x >= point[0] and event.x <= point[2]) and (event.y >= point[1] and event.y <= point[3]):
+			print('Exists', w.type(pt))
+			current = pt
+			return
+	print('Creating point')
+	createPoint(event)
 def listenDrag(event):
+	global current
 	print('Dragging', event.x, event.y)
+	print(current != None)
+	if current != None:
+		print('Dragging it!', event.x, event.y)
+		movePoint(event)
+		for pt in range(len(new)):
+			if current == new[pt]:
+				new_coords = getActualCoords(new[pt])
+				orig_coords = getActualCoords(original[pt])
+				old_coords = w.coords(arrows[pt])
+				w.coords(arrows[pt], old_coords[0], old_coords[1], new_coords[0], new_coords[1])
 def listenRelease(event):
+	global current
 	print('Releasing', event.x, event.y)
+	current = None
+
+# Create points
+def createPoint(event):
+	original.append(w.create_oval(event.x-9, event.y-9, event.x+9, event.y+9, width=0, fill="#ff0000",activefill="#ff0000",disabledfill="#ff0000"))
+	new.append(w.create_oval(event.x-9, event.y-9, event.x+9, event.y+9, width=0, fill="#00ff00"))
+	arrow = w.create_line(event.x, event.y, event.x, event.y, width=2, arrow=tkinter.LAST)
+	arrows.append(arrow)
+# Move point
+def movePoint(event):
+	w.coords(current, event.x-9, event.y-9, event.x+9, event.y+9)
+# Get points
+def getPoints():
+	return original, new
+# Get picture
+def getPicture():
+	return numpy.asarray(rimg1)
+def getActualCoords(point):
+	coords = w.coords(point)
+	return coords[0]+9, coords[1]+9
 
 # Initialize window and canvas
 top = tkinter.Tk()
@@ -39,6 +81,13 @@ img2 = ImageTk.PhotoImage(rimg2)
 # Create images
 w.create_image(0, 0, image=img1, anchor="nw")
 w.create_image(width,0, image=img2, anchor="nw")
+
+# Create points
+current = None
+new = []
+original = []
+arrows = []
+
 # w.pack(expand="yes", fill="both")
 w.pack()
 top.mainloop()	
